7b6f052e73fc35828ab93d28bb56439a8a667bd6
Thuan Van. Vo <votuonganvn@gmail.com>
2022-03-24 12:29:05 +0700

    Change 24-Mar-2022: - Initialize the repository - Update the template for coding files.

---
+++ Cuong/25_Mar_2022/Template.cpp
@@ -0,0 +1,11 @@
+/***************************************************************
+*Bai 1: Viet chuong trinh in ra man hinh dong chu "Hello World"*
+***************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello World";
+	return 0;
+}
