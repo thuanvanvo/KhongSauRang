c55d3e92e6fefcf1d9ab6db60a1b53d5fe5124ef
LE TRINH HUYNH VU <chaytron69@gmail.com>
2022-03-27 18:13:31 +0700

    Change 27-Mar-2022 - Submit BTVN_5.cpp

--- Vu/25_Mar_2022/BTVN_5.cpp
+++ Vu/25_Mar_2022/BTVN_5.cpp
@@ -1,30 +1,26 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    string hoten, ten;
+    string hoten, ten, xyz;
    int i,j;
    cout <<"Nhap ho va ten: ";
    getline(cin, hoten);
    for (i=hoten.size(); i>0; i--){
        if ('A' <= hoten[i] && hoten[i] <= 'Z'){
-           for (j = i; j <= hoten.size(); j++ ){
+           for (j = i; j <= hoen.size(); j++ ){
            ten = ten+hoten[j];
+           
            
            }
        }
    }
-    //le Trinh 
- 
+   for (i=0; i<hoten.size() - ten.size(); i++){
+      if ('A' <= hoten[i] && hoten[i] <= 'Z')
+      xyz = xyz + hoten[i];
+   }
+   cout<<ten <<xyz <<"@company.com";
     return 0;
 }
