38da7c8cbebe5ff95c734a1973cabfeec98085d7
LE TRINH HUYNH VU <chaytron69@gmail.com>
2022-03-28 22:57:29 +0700

    Change 28-Mar-2022 - Submit BTVN_1.cpp

---
+++ Vu/27_Mar_2022/BTVN_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[10]={2,3,1,6,5,9,8,2,11,3};
+    int n, count=0;
+    int i,j;
+    int sumOfAllMember=0, sumOfMember=0;
+   
+    for (i=0; i<10; i++){
+        sumOfAllMember+=a[i];
+    }
+    
+    cout << "Nhap so thu tu thanh vien trong dai doi: ";
+    for (i=0; i<i+1; i++){
+        cin >>n;
+        if (n <= 0 || n > sumOfAllMember){
+            cout <<"So thu tu thanh vien khong hop le, nhap lai: ";
+        }
+        else {
+            for (int j=0; j<10; j++){
+                sumOfMember+=a[j];
+                if (sumOfMember >=n){
+                    count = j+1;
+                    break;
+                }
+            }
+        cout <<"Thanh vien thu "<<n<<" thuoc Tieu doi "<<count;  
+        }
+      
+    }
+           
+    return 0;
+}
