3451aac4a6a31bbb786b255838423c3de601f6c1
binhganhep <binhgnguyen11021998@gmail.com>
2022-04-11 23:56:31 +0700

    Change 11-April-2022 - Submit  RentalRoomManagement_Project.cpp

--- Binh/RentalRoomManagement_Project/RentalRoomManagement_Project.cpp
+++ Binh/RentalRoomManagement_Project/RentalRoomManagement_Project.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <ctime>
 
-
 using namespace std;
 class PhongTro {
 private:
@@ -14,6 +13,42 @@ private:
 	unsigned int soDiencu;
 	unsigned int soNuoccu;
 public:
+	int getPhongSo() {
+		return phongSo;
+	}
+	void setPhongSo(int phongSo) {
+		this->phongSo = phongSo;
+	}
+	int getgiaTien() {
+		return giaTien;
+	}
+	void setgiaTien(int giaTien) {
+		this->giaTien = giaTien;
+	}
+	int getsoNuocmoi() {
+		return soNuocmoi;
+	}
+	void setsoNuocmoi(int soNuocmoi) {
+		this->soNuocmoi = soNuocmoi;
+	}
+	int getsoNuoccu() {
+		return soNuoccu;
+	}
+	void setsoNuoccu(int soNuoccu) {
+		this->soNuoccu = soNuoccu;
+	}
+	int getsoDienmoi() {
+		return soDienmoi;
+	}
+	void setsoDienmoi(int soDienmoi) {
+		this->soDienmoi = soDienmoi;
+	}
+	int getsoDiencu() {
+		return soDiencu;
+	}
+	void setsoDiencu(int soDiencu) {
+		this->soDiencu = soDiencu;
+	}
 	void DangKyPhong() {
 
 		cout << "so phong ";
@@ -28,28 +63,15 @@ public:
 		soDienmoi = soDiencu;
 		soNuocmoi = soNuoccu;
 	}
-	void UpdateNuocVaDien() {
-		soDiencu = soDienmoi;
-		soNuoccu = soNuocmoi;
-		cout << "cap nhat so dien moi: ";
-		cin >> soDienmoi;
-		cout << "cap nhat so nuoc moi: ";
-		cin >> soNuocmoi;
-	}
 	unsigned long int TinhTienDien() {
-		
-		return (soDienmoi - soDiencu) * 3500;
+		return(soDienmoi - soDiencu) * 3500;
 	}
 	unsigned long int TinhTienNuoc() {
-		return (soNuocmoi - soNuoccu) * 15000;
+	return(soNuocmoi - soNuoccu)* 15000;
 	}
 	unsigned long int TinhTongTienPhong() {
-		return giaTien + TinhTienNuoc() + TinhTienDien();
-	}
-	void ThongTinPhong() {
-		cout << "So phong:" << phongSo << "\n";
-		cout << "tien phong: " << giaTien <<"\n";
-	}
+	return giaTien + TinhTienDien() + TinhTienNuoc();
+}
 };
 class SoQuanLy {
 private:
@@ -58,7 +80,6 @@ private:
 public:
 	void KhoiTaoSo() {
 		int n;
-
 		cout << " nhap tong so phong: ";
 		cin >> n;
 		room.resize(n);
@@ -74,47 +95,84 @@ public:
 		PhongTro a;
 		a.DangKyPhong();
 		room.push_back(a);
-
+	}
+	void UpdateWaterAndElectric() {
+		int n;
+		int dienmoi;
+		int nuocmoi;
+		cout << "so phong can cap nhat so dien va so nuoc: " << "\n";
+		cin >> n;
+		for (int i = 0; i < room.size(); i++) {
+			if (room[i].getPhongSo() == n) {
+				room[i].setsoDiencu(room[i].getsoDienmoi());
+				cout << "so dien moi" << endl;
+				cin >> dienmoi;
+				room[i].setsoDienmoi(dienmoi);
+				break;
+			}
+			else cout << "so phong khong hop le";
+		}
+		for (int i = 0; i < room.size(); i++){
+				if (room[i].getPhongSo()==n)
+				}
+				room[i].setsoNuoccu(room[i].getsoNuocmoi());
+				cout << "so nuoc moi" << endl;
+				cin >> nuocmoi;
+				room[i].setsoNuocmoi(nuocmoi);
+				break;
+			}
+			else cout << "so phong khong hop le";
+		}
 	}
 	void HienThiTienDien() {
-		cout << "Nhap so thu tu cua phong: ";
+		cout << "Nhap so phong: ";
 		int chon;
 		cin >> chon;
-		if (chon <= 0 || chon > room.size()) cout << "so thu tu phong khong hop le";
-		else cout << "tien dien phong thu " << chon << "la : " << room[chon - 1].TinhTienDien();
-		system("pause");
+		for (int i = 0; i < room.size(); i++) {
+			if (room[i].getPhongSo() == chon) {
+				cout << "Phong so " << chon << "co tien dien la " << room[i].TinhTienDien() << endl;
+			}
+			else cout << "so phong khong hop le";
+		}
 	}
 	void HienThiTienNuoc() {
-		cout << "Nhap so thu tu cua phong: ";
+		cout << "Nhap so phong: ";
 		int chon;
 		cin >> chon;
-		if (chon <= 0 || chon > room.size()) cout << "so thu tu phong khong hop le";
-		else cout << "tien nuoc phong thu " << chon << "la : " << room[chon - 1].TinhTienNuoc();
-		system("pause");
-	}
-	void HienThiCapNhatDienNuoc() {
-		cout << "Nhap so thu tu cua phong: ";
-		int chon;
-		cin >> chon;
-		if (chon <= 0 || chon > room.size()) cout << "so thu tu phong khong hop le";
-		else cout << "dien va nuoc moi phong thu " << chon; "/n";
-		room[chon - 1].UpdateNuocVaDien();
-
+		for (int i = 0; i < room.size(); i++) {
+			if (room[i].getPhongSo() == chon) {
+				cout << "Phong so " << chon << "co tien dien la " << room[i].TinhTienNuoc() << endl;
+			}
+			else cout << "so phong khong hop le";
+		}
 	}
 	void HienThiHoaDonTienNha() {
-		cout << "Nhap so thu tu cua phong: ";
+		cout << "Nhap so cua phong: can xuat hoa don tien nha\n";
 		int chon;
 		cin >> chon;
 		time_t now = time(0);
 		tm* ltm = localtime(&now);
-		if (chon <= 0 || chon > room.size()) cout << "so thu tu phong khong hop le";
-		else { 
-			cout << "hoa don tien nha thang " << 1 + ltm->tm_mon << "\n";
-			cout << "thong tin phong" << "\n";
-			room[chon - 1].ThongTinPhong();
-			cout << "tien dien " << room[chon - 1].TinhTienDien() << "\n";
-			cout << "tien nuoc " << room[chon - 1].TinhTienNuoc() << "\n";
-			cout << "hoa don tien phong " << chon << "la : " << room[chon - 1].TinhTongTienPhong();
+		for (int i = 0; i < room.size(); i++) {
+			if (room[i].getPhongSo() == chon) {
+				cout << "Hoa don tien nha thang " << 1 + ltm->tm_mon << " nam " << 1900 + ltm->tm_year << "\n";
+				cout << "phong so: " << room[i].getPhongSo() << endl;
+				cout << "gia phong: " << room[i].getgiaTien() << endl;
+				cout << "tien dien " << room[i].TinhTienDien() << endl;
+				cout << "tien nuoc " << room[i].TinhTienNuoc() << endl;
+				cout << "hoa don tien phong " << chon << "la : " << room[i].TinhTongTienPhong() << endl;
+				system("pause");
+			}
+			else cout << "so phong khong hop le";
+		}
+	}
+	void MaxPriceElectric() {
+		int Max = 0;
+		int roomMax = 0;
+		for (int i = 0; i < room.size(); i++) {
+			if (room[i].TinhTienDien() > Max)
+				Max = room[i].TinhTienDien();
+			roomMax = i + 1;
+			cout << "phong co tien dien nhieu nhat la: " << roomMax << "\n";
 			system("pause");
 		}
 	}
@@ -129,18 +187,20 @@ public:
 			cout << "4. tien dien phong\n";
 			cout << "5. tien nuoc phong\n";
 			cout << "6. hoa don tien phong\n";
-			cout << "7. thoat\n";
+			cout << "7. Phong xai dien hao nhat\n";
+			cout << "8. thoat\n";
 			cout << "vui long nhap lua chon";
 			cin >> chon;
 			switch (chon) {
 			case 1: KhoiTaoSo(); break;
 			case 2: ThemPhong(); break;
-			case 3: HienThiCapNhatDienNuoc(); break;
+			case 3: UpdateWaterAndElectric(); break;
 			case 4: HienThiTienDien(); break;
 			case 5: HienThiTienNuoc(); break;
 			case 6: HienThiHoaDonTienNha(); break;
+			case 7: MaxPriceElectric(); break;
 			}
-		} while (chon != 7);
+		} while (chon != 8);
 	}
 };
 int main() {
